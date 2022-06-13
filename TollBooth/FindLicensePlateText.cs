499a4c3e8656c3d99fa7bfadbb43661aa7de85e2
JeroenVanTiel <jjmvantiel@jumbo.com>
2022-06-13 14:03:58 -0700

    Update

--- TollBooth/FindLicensePlateText.cs
+++ TollBooth/FindLicensePlateText.cs
@@ -44,7 +44,6 @@ namespace TollBooth
             const string requestParameters = "language=unk&detectOrientation=true";
             // Get the API URL and the API key from settings.
    
-            // TODO 2: Populate the below two variables with the correct AppSettings properties.
             var uriBase = Environment.GetEnvironmentVariable("computerVisionApiUrl");
             var apiKey = Environment.GetEnvironmentVariable("computerVisionApiKey");
 
